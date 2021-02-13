8a5bd889d0d01e9b6cdfb8f0b2a748b8a9be9482
Wanish Sengkhim <wanish45037.2@gmail.com>
2021-02-13 23:49:15 +0700

    first commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Admin\Desktop\Y3\CoE 3 Term 2\Advance Lab II\3SB04\Django_Framework\myvenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
